a918a1fad2fabf84178624fc38e546d302d2b1b0
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2025-10-06 15:16:02 -0300

    Criação da classe ClienteDAO

---
+++ src/main/java/br/barbearia/posttest/ClienteDAO.java
@@ -0,0 +1,15 @@
+package br.barbearia.posttest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class ClienteDAO {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/barbearia_db";
+    private static final String USUARIO = "barbearia";
+    private static final String SENHA = "2001310post";
+
+}
