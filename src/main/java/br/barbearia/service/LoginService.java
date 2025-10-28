536a9923268b79ac2db03579cde8695f66c5a5db
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2025-10-28 13:26:12 -0300

    Correção da LoginService para que ela se conecte com o repositório.

--- src/main/java/br/barbearia/service/LoginService.java
+++ src/main/java/br/barbearia/service/LoginService.java
@@ -7,24 +7,28 @@ import br.barbearia.repository.UsuarioRepository;
 
 public class LoginService {
 
-    private UsuarioRepository usuarioRepository = new UsuarioRepository();
+    private UsuarioRepository usuarioRepository;
 
-    public Cliente cadastrarUsuario(Usuarios novoUsuario)throws Exception{
-        if(novoUsuario.getNome() == null ||novoUsuario.getNome().trim().isEmpty()){
+    public LoginService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuarios cadastrarUsuario(Usuarios novoUsuario)throws Exception {
+        if (novoUsuario.getNome() == null || novoUsuario.getNome().trim().isEmpty()) {
             throw new Exception("Existem campos obrigátorios que não foram preenchidos");
         }
-        if(novoUsuario.getCpf() == null || novoUsuario.getCpf().replaceAll("[^0-9]", "").length()!=11){
+        if (novoUsuario.getCpf() == null || novoUsuario.getCpf().replaceAll("[^0-9]", "").length() != 11) {
             throw new Exception("Coloque o formato correto para CPF");
         }
-        if(novoUsuario.getTelefone() == null || novoUsuario.getTelefone().trim().isEmpty()) {
+        if (novoUsuario.getTelefone() == null || novoUsuario.getTelefone().trim().isEmpty()) {
             throw new Exception("Preencha o campo TELEFONE");
         }
-        if(usuarioRepository.buscarUsuarioPorCpf(novoUsuario.getCpf())!=null){
+        if (usuarioRepository.buscarUsuarioPorCpf(novoUsuario.getCpf()) != null) {
             throw new Exception("Este CPF já está cadastrado");
         }
         System.out.println("Todas as validações foram atendidas. Cliente salvo.");
-        //return clientesRepository.salvarDados(novoCliente);
-        return null;
+        usuarioRepository.adicionarUsuario(novoUsuario);
+        return novoUsuario;
 
 
     }
