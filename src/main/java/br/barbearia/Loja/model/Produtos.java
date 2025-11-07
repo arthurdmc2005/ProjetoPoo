b453bb5594e48c09de33dbf3ee1c480c6e0f533e
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2025-11-06 23:41:23 -0300

    Criando os atributos da classe de Produtos

---
+++ src/main/java/br/barbearia/Loja/model/Produtos.java
@@ -0,0 +1,63 @@
+package br.barbearia.Loja.model;
+
+public class Produtos {
+
+    private String nomeProduto;
+    private double quantidade;
+    private String fornecedor;
+    private int id;
+
+
+
+    public Produtos(String nomeProduto, double quantidade, String fornecedor, int id) {
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+        this.fornecedor = fornecedor;
+        this.id = id;
+    }
+
+    public Produtos(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "EstoqueModel{" +
+                "nomeProduto='" + nomeProduto + '\'' +
+                ", quantidade=" + quantidade +
+                ", fornecedor='" + fornecedor + '\'' +
+                ", id=" + id +
+                '}';
+    }
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+}
